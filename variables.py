18d2302d04348982c5a0ded65a9acf65a0da91ca
unknown <inha.virza@gmail.com>
2025-03-28 22:36:57 +0200

    variables

---
+++ variables.py
@@ -0,0 +1,11 @@
+# snake_case - variables, function, method, models
+# PascalCase - class
+# my-package - packet (combined modules)
+# DB_PASSWORD - const
+
+
+#Dynamic typing
+my_variable = 10
+my_variable = 'Inga'
+my_variable = True
+
