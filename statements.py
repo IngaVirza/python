4a15a33ba24862fc558ecac8ddb1b884914c5e09
unknown <inha.virza@gmail.com>
2025-03-28 22:39:43 +0200

    statements

---
+++ statements.py
@@ -0,0 +1,10 @@
+#my_name = 'Inga' # Value assignments
+#if my_name: 
+#    print(my_name) #Conditional instruction
+
+#import datetime #Importing a module
+
+
+import datetime
+
+print(datetime.MAXYEAR)
