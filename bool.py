ba5fe94059f2b4f405be09bc87bf31ee4b9ea5ab
unknown <inha.virza@gmail.com>
2025-05-14 21:13:43 +0300

    bool

---
+++ bool.py
@@ -0,0 +1,32 @@
+is_authorized = True
+
+print(is_authorized)
+#True
+print( not is_authorized)
+#False
+print(type(is_authorized))
+#<class 'bool'>
+
+x=10
+y=10
+print(x ==y)
+#True
+is_equel = x == y 
+print(is_equel)
+#True
+not_equel = x!= y
+print(not_equel)
+#False
+z = 6
+print(z < 5 and z>-2)
+#False
+print(bool(4))
+#True
+print(bool(0))
+#False
+print(bool(-2))
+#True
+print(bool("Inga"))
+#TRUE
+print(bool(""))
+#False
