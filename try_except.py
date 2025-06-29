38f26376c8777f919a3496aeb83f80b374964fd3
unknown <inha.virza@gmail.com>
2025-06-30 01:37:11 +0300

    try_except

---
+++ try_except.py
@@ -0,0 +1,26 @@
+def find_average(*, numbers: list) -> float: 
+    return sum(numbers)/ len(numbers)
+
+print(find_average(numbers=[1,2,3,4,5]))
+#3.0
+
+#print(find_average(numbers=[]))
+
+# return sum(numbers)/ len(numbers)          
+#ZeroDivisionError: division by zero
+
+def find_average(*, numbers: list) -> float: 
+    return sum(numbers)/ len(numbers)
+
+try:
+    find_average(numbers=[])
+except ZeroDivisionError:
+    print("The list is empty")    
+
+print("we are here")
+
+
+#The list is empty
+#we are here
+
+
